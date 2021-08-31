69e8c9b6673c5561dcbea3e0c189a9cc05bdb3fd
nino-0 <manoelvitor@acad.ifma.edu.br>
2021-08-30 22:00:23 -0300

    socket serve finalizado

--- chat/client.py
+++ chat/client.py
@@ -1,38 +1,57 @@
 import socket
 import threading
+import time
 
+FORMAT = "utf-8"
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('138.97.233.2', 55555))
+client.connect(('127.0.0.1', 55555))
 
+# '192.168.100.102'
 # Listening to Server and Sending Nickname
+
+
 def receive():
     while True:
         try:
             # Receive Message From Server
             # If 'NICK' Send Nickname
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
+            message = client.recv(1024)
+            msg_decode = message.decode('ascii')
+
+            if msg_decode == 'NICK':
                 client.send(nickname.encode('ascii'))
             else:
-                print(message)
+                print(msg_decode)
+                if msg_decode == 'exec':
+
+                    print(f" o cliente {nickname} esta executando....")
+                    # executar o script RPA
+                    time.sleep(10)
+                    client.send(f"[{nickname}]:done".encode('utf-8'))
+                    print("execução finalizada")
+
         except:
             # Close Connection When Error
             print("An error occured!")
             client.close()
             break
 # Sending Messages To Server
+
+
 def write():
     while True:
-        message = '{}: {}'.format(nickname, input(''))
+        print("digite algo: ", end='')
+        message = '[{}]:{}'.format(nickname, input(''))
         client.send(message.encode('ascii'))
+
 
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+# write_thread = threading.Thread(target=write)
+# write_thread.start()
--- chat/server.py
+++ chat/server.py
@@ -2,7 +2,9 @@ import socket
 import threading
 
 # Connection Data
-host = '127.0.0.1'
+# socket.gethostbyname(socket.gethostname())  # '127.0.0.1'
+host = '127.0.0.1'  # socket.gethostbyname(socket.gethostname())
+FORMAT = 'utf-8'
 port = 55555
 
 # Starting Server
@@ -13,30 +15,66 @@ server.listen()
 # Lists For Clients and Their Nicknames
 clients = []
 nicknames = []
-
+fila = []
 # Sending Messages To All Connected Clients
+
+
 def broadcast(message):
+
     for client in clients:
         client.send(message)
 
+# envia uma msg para o cliente executar o RPA
+
+
+def send_exec_cliente():
+    while True:
+        print("digite 'exec' para inciar um execuçao de robô")
+        msg = input('')
+        if len(fila) > 0:
+            na_vez = fila[0]
+
+            na_vez.send(msg.encode('ascii'))
+            fila.pop(0)
+        else:
+            print("não há ninguem conectado no server")
 # Handling Messages From Clients
+
+
 def handle(client):
     while True:
         try:
             # Broadcasting Messages
+
             message = client.recv(1024)
-            broadcast(message)
+            msg_decode = message.decode(FORMAT)
+            cliente_nome = msg_decode[msg_decode.find(
+                "[")+1:msg_decode.find("]")]
+            content = msg_decode[msg_decode.find(":")+1:]
+            if content == "done":
+                print('tamanho da fila', len(fila))
+                print(f"{cliente_nome} Finalizou a execução")
+                fila.append(client)
+                print('tamanho da fila', len(fila))
+                # adiciona o cliente no final da fila
+
+            # broadcast(message)
         except:
             # Removing And Closing Clients
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('ascii'))
+            # broadcast('{} left!'.format(nickname).encode(FORMAT))
             nicknames.remove(nickname)
             break
 
 # Receiving / Listening Function
+
+# pegando valor entre cochete. y nome da str
+# name = y[y.find("[")+1:y.find("]")]
+
+
 def receive():
     while True:
         # Accept Connection
@@ -48,13 +86,20 @@ def receive():
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
-
+        fila.append(client)
         # Print And Broadcast Nickname
         print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
+        # broadcast("{} joined!".format(nickname).encode('ascii'))
+        # client.send('Connected to server!'.encode('ascii'))
 
         # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-receive()
+
+
+# receive()
+write_thread = threading.Thread(target=receive)
+write_thread.start()
+print("server is runing")
+write_thread = threading.Thread(target=send_exec_cliente)
+write_thread.start()
